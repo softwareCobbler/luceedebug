47bcbe710e7a8ea679585cda19697efe901aeb33
David Rogers <davidwrogers2@gmail.com>
2023-04-21 18:56:17 -0500

    byte wrangling

--- jdwp-proxy/src/main/scala/Xxx.scala
+++ jdwp-proxy/src/main/scala/Xxx.scala
@@ -1,6 +1,6 @@
-package luceedebug.jdwp_proxy
+package XXX
 
-import luceedebug.jdwp_proxy.JdwpProxy.{HANDSHAKE_BYTES, HANDSHAKE_STRING}
+import JdwpProxy.{HANDSHAKE_BYTES, HANDSHAKE_STRING}
 
 import java.io.{IOException, InputStream, OutputStream}
 import java.net.{InetAddress, InetSocketAddress, Socket}
@@ -10,11 +10,13 @@ import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration.*
 import scala.util.{Failure, Success, Try}
+import scala.util.control.Breaks._
 import scala.annotation.tailrec
 import scala.collection.mutable.HashMap
 import java.util.concurrent.atomic.AtomicInteger
-import luceedebug.jdwp_proxy.JdwpProxy.IProxyClient
 import java.net.ServerSocket
+import scala.math.{min}
+import scala.util.control.Exception.By
 
 enum Foo:
   case X(v: Any)
@@ -22,15 +24,13 @@ enum Foo:
 
 @main
 def ok() : Unit =
-  val x = new Array[Byte](5)
-  println(x.slice(0,45)(5));
-  //JdwpProxy().connect("localhost", 9999)//println(CheckedReader("xpabx".getBytes(StandardCharsets.UTF_8)).readUtf8("xpab"))
+  JdwpProxy(host="localhost", port=9999)
 
 
 abstract class ISocketIO {
   def chunkedReader(chunkSize: Int) : () => Array[Byte]
   def write(bytes: Array[Byte], timeout_ms: Int) : Unit
-  def read(n: Int, timeout_ms: Int) : Unit
+  def read(n: Int, timeout_ms: Int) : Array[Byte]
 }
 
 class SocketIO(private val inStream: InputStream, private val outStream: OutputStream) extends ISocketIO {
@@ -58,36 +58,53 @@ class SocketIO(private val inStream: InputStream, private val outStream: OutputS
       else bytes
 }
 
-// java is big endian, network is big endian
+/**
+ * java is big endian, network is big endian
+ */
 class CheckedReader(raw: Array[Byte]) {
   private var index : Int = 0
 
   private def getAndAdvance(len: Int) : Array[Byte] =
-    if index + len >= raw.length
-    then throw new RuntimeException("out of bounds buffer access")
     val ret = raw.slice(index, index + len)
+    ret(len - 1) // dies on out-of-bounds right? (slice allows out-of-bounds slices...?)
     index += len
     ret
 
   def readN(n: Int) : Array[Byte] = getAndAdvance(n)
+  def read_int8: Byte = ByteWrangler.beI8_to_int8(readN(1))
+  def read_int16: Short = ByteWrangler.beI16_to_int16(readN(2))
+  def read_int32 : Int = ByteWrangler.beI32_to_int32(readN(4))
+}
 
-  def read_int8: Byte =
-    val span = getAndAdvance(1)
-    span(0)
+object ByteWrangler {
+  def int8_to_beI8(v: Byte) : Array[Byte] = Array[Byte](v)
 
-  def read_int16: Short =
-    val span = getAndAdvance(2)
-    val ret =
-      (span(0) << 8)
-        | (span(1) << 0)
-    ret.asInstanceOf[Short]
+  def beI8_to_int8(vs: Array[Byte]) : Byte = vs(0)
 
-  def read_int32 : Int =
-    val span = getAndAdvance(4)
-    (span(0) << 24)
-      | (span(1) << 16)
-      | (span(2) << 8)
-      | (span(3) << 0)
+  def int16_to_beI16(v: Short) : Array[Byte] =
+    Array[Byte](
+      ((v >>> 8) & 0xFF).asInstanceOf[Byte],
+      ((v >>> 0) & 0xFF).asInstanceOf[Byte],
+    )
+
+  def beI16_to_int16(vs: Array[Byte]) : Short = 
+    val hi = (vs(0) & 0xFF) << 8
+    val lo = (vs(1) & 0xFF) << 0
+    (hi | lo).asInstanceOf[Short]
+
+  def int32_to_beI32(v: Int) : Array[Byte] =
+    Array[Byte](
+      ((v >>> 24) & 0xFF).asInstanceOf[Byte],
+      ((v >>> 16) & 0xFF).asInstanceOf[Byte],
+      ((v >>> 8) & 0xFF).asInstanceOf[Byte],
+      ((v >>> 0) & 0xFF).asInstanceOf[Byte],
+    )
+
+  def beI32_to_int32(vs: Array[Byte]) : Int = 
+    ((vs(0) & 0xFF) << 24)
+      | ((vs(1) & 0xFF) << 16)
+      | ((vs(2) & 0xFF) << 8)
+      | ((vs(3) & 0xFF) << 0)
 }
 
 class LuceeDebugJdwpProxy(
@@ -163,7 +180,7 @@ class JdwpProxyManager(jvmHost: String, jvmPort: Int) {
 class ClientFacingProxyClient(
   val id: Int,
   val io: ISocketIO,
-  val receiveFromClient: Array[Byte] => Unit,
+  val receiveFromClient: (client: ClientFacingProxyClient, bytes: Array[Byte]) => Unit,
   val receiveFromJVM: Array[Byte] => Unit,
   val onClientDisconnect : () => Unit,
 ) {
@@ -171,7 +188,7 @@ class ClientFacingProxyClient(
     val thread = new Thread(() => {
       val reader = io.chunkedReader(1024)
       while true do
-        receiveFromClient(reader())
+        receiveFromClient(this, reader())
     })
     thread.start()
     thread
@@ -199,9 +216,8 @@ class JdwpProxy(host: String, port: Int) {
   class PacketOrigin(val originalID: Int, val client: ClientFacingProxyClient)
 
   private val nextClientID = AtomicInteger()
-  private val jvmSocketIO = JdwpProxy.getSocketIOFromHandshake(host, port)
+  private val (jvmSocketIO, parser, idSizes) = JdwpProxy.getSocketIOFromHandshake(host, port)
   private val jvm : JvmFacingProxyClient = {
-    val parser = PacketParser()
     JvmFacingProxyClient(
       id = nextClientID.getAndIncrement(),
       io = jvmSocketIO,
@@ -234,7 +250,7 @@ class JdwpProxy(host: String, port: Int) {
     val client : ClientFacingProxyClient = ClientFacingProxyClient(
       id = nextClientID.getAndIncrement(),
       io = socketIO,
-      receiveFromClient = bytes => {
+      receiveFromClient = (client, bytes) => {
         for (packet <- parser.consume(bytes)) do
           jvm.receiveFromClient(client, packet)
       },
@@ -252,7 +268,7 @@ object JdwpProxy {
   private val HANDSHAKE_BYTES: Array[Byte] = HANDSHAKE_STRING.getBytes(StandardCharsets.US_ASCII)
   
   @throws(classOf[IOException])
-  private def getSocketIOFromHandshake(host: String, port: Int) : SocketIO =
+  private def getSocketIOFromHandshake(host: String, port: Int) : (SocketIO, PacketParser, packet.reply.IdSizes) =
     val socket = new Socket()
     val inetAddr = new InetSocketAddress(host, port);
     socket.connect(inetAddr, /*ms*/5000);
@@ -262,11 +278,32 @@ object JdwpProxy {
     val handshakeIn = socketIO.read(HANDSHAKE_STRING.length, 5000)
     val receivedHandshake = String(CheckedReader(handshakeIn).readN(HANDSHAKE_STRING.length), StandardCharsets.UTF_8)
 
-    // do the "size" message first?
+    if !receivedHandshake.equals(HANDSHAKE_STRING)
+    then throw new IOException(s"Bad JDWP handshake, got '${receivedHandshake}'")
 
-    if receivedHandshake.equals(HANDSHAKE_STRING)
-    then socketIO
-    else throw new IOException(s"Bad JDWP handshake, got '${receivedHandshake}'")
+    // do the "size" message first?
+    socketIO.write(
+      bytes = CommandPacket.toWire(0, packet.command.IdSizes()),
+      timeout_ms = 5000
+    )
+    
+    val parser = PacketParser()
+    val parsed = ArrayBuffer[rawpacket.FinishedPacket]()
+    val chunkedReader = socketIO.chunkedReader(128)
+    var idSizes : Option[packet.reply.IdSizes] = None
+    breakable {
+      import packet.reply.IdSizes
+      while true do
+        for (packet <- parser.consume(chunkedReader())) do
+          if packet.ID == 0
+          then
+            idSizes = Some(IdSizes.fromWire(packet.raw))
+            break
+          else parsed += packet
+    }
+
+    (socketIO, parser, idSizes.get)
+    
 }
 
 package rawpacket:
@@ -360,6 +397,7 @@ package rawpacket:
   }
 
 class PacketParser {
+  private val HEADER_LENGTH = 11
   enum State:
     case HeaderLength()
     case HeaderID()
@@ -379,51 +417,55 @@ class PacketParser {
     builder.raw += byte
   
   private def parseLength(buffer: Array[Byte]) : ParseResult =
-    var remaining = 4 - builder.length.length
+    val consumable = min(buffer.length, 4 - builder.length.length)
+    var consumed = 0
     val checkedReader = CheckedReader(buffer)
-    while remaining > 0 do
+    while consumed != consumable do
       pushByte(builder.length, checkedReader.read_int8)
-      remaining -= 1
+      consumed += 1
 
     ParseResult(
       nextState = if builder.length.length == 4 then State.HeaderID() else State.HeaderLength(),
-      remaining = buffer.slice(remaining, buffer.length)
+      remaining = buffer.slice(consumed, buffer.length)
     )
 
   private def parseId(buffer: Array[Byte]) : ParseResult =
-    var remaining = 4 - builder.id.length
+    var consumable = min(buffer.length, 4 - builder.id.length)
+    var consumed = 0
     val checkedReader = CheckedReader(buffer)
-    while remaining > 0 do
+    while consumed != consumable do
       pushByte(builder.id, checkedReader.read_int8)
-      remaining -= 1
+      consumed += 1
 
     ParseResult(
       nextState = if builder.id.length == 4 then State.HeaderLast3() else State.HeaderID(),
-      remaining = buffer.slice(remaining, buffer.length)
+      remaining = buffer.slice(consumed, buffer.length)
     )
 
   private def parseLast3(buffer: Array[Byte]) : ParseResult =
-    var remaining = 3 - builder.last3.length
+    val consumable = min(buffer.length, 3 - builder.last3.length)
+    var consumed = 0
     val checkedReader = CheckedReader(buffer)
-    while remaining > 0 do
+    while consumed != consumable do
       pushByte(builder.last3, checkedReader.read_int8)
-      remaining -= 1
+      consumed += 1
 
     ParseResult(
       nextState = if builder.last3.length == 3 then State.Body() else State.HeaderLast3(),
-      remaining = buffer.slice(remaining, buffer.length)
+      remaining = buffer.slice(consumable, buffer.length)
     )
   
   private def parseBody(buffer: Array[Byte]) : ParseResult =
-    var remaining = builder.readLength() - builder.length.length
+    val consumable = min(buffer.length, builder.readLength() - builder.length.length)
+    var consumed = 0
     val checkedReader = CheckedReader(buffer)
-    while remaining > 0 do
+    while consumed != consumable do
       pushByte(builder.body, checkedReader.read_int8)
-      remaining -= 1
+      consumed += 1
 
     ParseResult(
-      nextState = if builder.body.length == builder.readLength() then State.Done() else State.Body(),
-      remaining = buffer.slice(remaining, buffer.length)
+      nextState = if builder.body.length + HEADER_LENGTH == builder.readLength() then State.Done() else State.Body(),
+      remaining = buffer.slice(consumed, buffer.length)
     )
 
   private def unreachable[T] : T = throw RuntimeException("unreachable")
@@ -438,13 +480,16 @@ class PacketParser {
 
   @tailrec
   private def consume(buffer: Array[Byte], completed: List[rawpacket.FinishedPacket]) : List[rawpacket.FinishedPacket] =
+    if buffer.length == 0
+    then return completed
+
     parse(buffer) match
       case ParseResult(State.Done(), remaining) =>
         state = startState
         consume(remaining, completed :+ builder.toFinishedPacket())
       case ParseResult(nextState, remaining) =>
         state = nextState
-        completed
+        consume(remaining, completed)
 
   def consume(buffer: Array[Byte]) : List[rawpacket.FinishedPacket] = consume(buffer, List())
 }
@@ -454,15 +499,27 @@ class JdwpCommandHeader{}
 class JdwpReplyHeader(val length: Int, val id: Int, val flags: Byte, val errorCode: Short){}
 
 object CommandPacket {
-  def toWire(id: Int, flags: Int, command: JdwpCommand) : Array[Byte] =
-    val body = command.toWire
+  def toWire(id: Int, command: JdwpCommand) : Array[Byte] =
+    val body = command.bodyToWire
     val length = body.length + 11;
-    val flags = 0
-    val commandSetID = command.command.getCommentSetID
+    val commandSetID = command.command.getCommandSetID
     val commandID = command.command.getCommandID
-    val header = new Array[Byte](0)
+    val header = Array[Byte](
+      (length & 0xFF000000 >> 24).asInstanceOf[Byte],
+      (length & 0x00FF0000 >> 16).asInstanceOf[Byte],
+      (length & 0x0000FF00 >> 8).asInstanceOf[Byte],
+      (length & 0x000000FF >> 0).asInstanceOf[Byte],
+      (id & 0xFF000000 >> 24).asInstanceOf[Byte],
+      (id & 0x00FF0000 >> 16).asInstanceOf[Byte],
+      (id & 0x0000FF00 >> 8).asInstanceOf[Byte],
+      (id & 0x000000FF >> 0).asInstanceOf[Byte],
+      0.asInstanceOf[Byte],
+      commandSetID,
+      commandID
+    )
     header ++ body
 }
+
 
 object ReplyPacket {
   def fromWire(bytes: Array[Byte]) : Array[Byte] =
@@ -479,24 +536,24 @@ object ReplyPacket {
     )
 }
 
-enum Command(commandSetID: Int, commandID: Int):
-  def getCommentSetID = commandSetID
+enum Command(commandSetID: Byte, commandID: Byte):
+  def getCommandSetID = commandSetID
   def getCommandID = commandID
   case VirtualMachine_IDSizes extends Command(1, 7)
 
 trait JdwpCommand {
   val command: Command
-  def toWire : Array[Byte]
+  def bodyToWire : Array[Byte]
 }
 
 trait FromWire[T] {
   def fromWire(buffer: Array[Byte]): T
 }
 
-package packet.out:
+package packet.command:
   class IdSizes() extends JdwpCommand {
     final val command = Command.VirtualMachine_IDSizes
-    def toWire = new Array[Byte](0)
+    def bodyToWire = new Array[Byte](0)
   }
 
 package packet.reply:
