4633f9c7ca43efc360cf1423bfee384671d0283e
David Rogers <davidwrogers2@gmail.com>
2025-03-12 18:10:36 -0500

    Strong<String> should override toString()

--- luceedebug/src/main/java/luceedebug/strong/CanonicalServerAbsPath.java
+++ luceedebug/src/main/java/luceedebug/strong/CanonicalServerAbsPath.java
@@ -4,4 +4,9 @@ public final class CanonicalServerAbsPath extends StrongT<String> {
     public CanonicalServerAbsPath(String v) {
         super(v);
     }
+
+    @Override
+    public String toString() {
+        return this.get();
+    }
 }
--- luceedebug/src/main/java/luceedebug/strong/RawIdePath.java
+++ luceedebug/src/main/java/luceedebug/strong/RawIdePath.java
@@ -4,4 +4,9 @@ public final class RawIdePath extends StrongT<String> {
     public RawIdePath(String v) {
         super(v);
     }
+
+    @Override
+    public String toString() {
+        return this.get();
+    }
 }
